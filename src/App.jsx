25f007766b02d3195cd5f8d758aac44f33cbd030
Varun Kodamanchili <varun132413@gmail.com>
2025-01-25 23:40:54 +0530

    ui modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
-import { Button } from 'semantic-ui-react'
+import { Button,Header } from 'semantic-ui-react'
 import { useState,useEffect } from "react";
+import photo from './assets/static.png';
 function App() {
   //javascript
   const [coin,setCoin] = useState("");
@@ -13,36 +14,45 @@ function App() {
   }
   return(
     <div className="main">
-      <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/401dde00-760d-406d-92ac-86ebaab75368/dbkp9bn-b742f397-4b29-4b96-901d-5dc638eee275.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQwMWRkZTAwLTc2MGQtNDA2ZC05MmFjLTg2ZWJhYWI3NTM2OFwvZGJrcDlibi1iNzQyZjM5Ny00YjI5LTRiOTYtOTAxZC01ZGM2MzhlZWUyNzUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.hZD6eeEunqAO9QOann6l4kOCDGMTd8QJMHtwttuShq8" alt="" id="image"/>
-      <Button inverted color='green' onClick={()=>{
+      <img src={photo} alt="" id="static"/>
+      <img src="https://media1.giphy.com/media/MOsuJf3qp3b1fQx2Iv/200w.gif?cid=6c09b9527kutfhdln2w6het3enu4safwej0t6hams09ncfkc&ep=v1_stickers_search&rid=200w.gif&ct=s" alt="" id="image"/>
+      <Button color='yellow' onClick={()=>{
         let image = document.getElementById("image");
+        let staticImage = document.getElementById("static");
         image.style.display = "block";
+        staticImage.style.display = "none";
         setTimeout(()=>{
           tossFun();
           image.style.display = "none";
+          staticImage.style.display = "block";
         },3000)
       }}>
         Toss
       </Button>
-      <h1>{coin}</h1>
-
+      <Header size='huge'>{coin}</Header>
       <div className="transparent">
       <button basic color='grey' content='Tail' onClick={()=>{
         let image = document.getElementById("image");
+        let staticImage = document.getElementById("static");
         image.style.display = "block";
+        staticImage.style.display = "none";
         setTimeout(()=>{
           setCoin("Tails");
           image.style.display = "none";
+          staticImage.style.display = "block";
         },3000);
-      }}></button>
+      }} id="button"></button>
       <button content='head' onClick={()=>{
         let image = document.getElementById("image");
+        let staticImage = document.getElementById("static");
         image.style.display = "block";
+        staticImage.style.display = "none";
         setTimeout(()=>{
           setCoin("Heads");
           image.style.display = "none";
+          staticImage.style.display = "block";
         },3000);
-      }}></button>
+      }} id="button"></button>
       </div>
     </div>
     
